b5a43f615dc0a30615f2ab5b6d4b549dde604692
TuanPham <tuanpntse173039@fpt.edu.vn>
2025-03-13 17:37:31 +0700

    test commit in current branch

--- src/pages/UserPage.tsx
+++ src/pages/UserPage.tsx
@@ -4,6 +4,7 @@ const UserPage: React.FC = () => {
   return (
     <div style={{ padding: '20px' }}>
       <h1>User Management</h1>
+      <h2>Hello User list</h2>
       <UserList />
     </div>
   );
