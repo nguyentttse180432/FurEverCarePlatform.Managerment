fc9d2f0c322c8a6d8d5eb53f627defe176086edb
TuanPham <tuanpntse173039@fpt.edu.vn>
2025-03-17 20:53:25 +0700

    Add environment variable for backend URL and update axios client configuration

--- src/services/clients.ts
+++ src/services/clients.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 export const client = axios.create({
-    baseURL : 'http://localhost:5225/api/v1',
-    headers: {
-        'Content-Type': 'application/json'
-    }
-})
+  baseURL: `${import.meta.env.VITE_BACKEND_URL}`,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
